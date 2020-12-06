dd628137e8a94cda4afbeccf95327d2cb357ad53
Russell Matney <russell.matney@gmail.com>
2020-12-06 01:13:45 -0500

    feat: day 6!
    it's quiet.... too quiet....

---
+++ src/six/core.clj
@@ -0,0 +1,32 @@
+(ns six.core
+  (:require [util :refer [input]]
+            [clojure.string :as string]
+            [clojure.set :as set]))
+
+(comment
+
+  ;; part 1
+  (->>
+    (input "input.txt")
+    (partition-by #{""})
+    (remove (comp #{""} first))
+    (map string/join)
+    (map set)
+    (map count)
+    (apply +))
+
+
+  ;; part 2
+  (->>
+    (input "input.txt")
+    (partition-by #{""})
+    (remove (comp #{""} first))
+    (map (fn [answers]
+           (->> answers
+                (map set)
+                (apply set/intersection))))
+    (map count)
+    (apply +)
+    )
+
+  )

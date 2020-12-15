0773a4c11d2f691bb2bb6049ac83aec29021f610
Russell Matney <russell.matney@gmail.com>
2020-12-15 15:04:16 -0500

    wip part 2

--- src/twelve/core.clj
+++ src/twelve/core.clj
@@ -14,7 +14,7 @@
   (parse "input.txt")
   )
 
-(def commands
+(def commands-part-1
   {"N" (fn [{:keys [position facing]} num]
          {:position {:x (:x position)
                      :y (+ (:y position) num)}
@@ -38,10 +38,10 @@
    "F" (fn [{:keys [facing] :as agg} num]
          {:position
           (:position (case (mod facing 360)
-                       0   ((commands "E") agg num)
-                       90  ((commands "S") agg num)
-                       180 ((commands "W") agg num)
-                       270 ((commands "N") agg num)))
+                       0   ((commands-part-1 "E") agg num)
+                       90  ((commands-part-1 "S") agg num)
+                       180 ((commands-part-1 "W") agg num)
+                       270 ((commands-part-1 "N") agg num)))
           :facing facing})
 
    "L" (fn [{:keys [position facing]} num]
@@ -56,25 +56,88 @@
   (mod 540 360)
   )
 
-(defn travel [f]
-  (->> (parse f)
-       (reduce
-         (fn [agg {:keys [letter num]}]
-           (println agg)
-           ((commands letter) agg num))
-         {:position {:x 0 :y 0}
-          :facing   0})))
+(defn travel
+  ([f commands] (travel f commands nil))
+  ([f commands waypoint]
+   (->> (parse f)
+        (reduce
+          (fn [agg {:keys [letter num]}]
+            (println agg)
+            ((commands letter) agg num))
+          {:position {:x 0 :y 0}
+           :waypoint waypoint
+           :facing   0}))))
 
 (comment
-  (->> (travel "example.txt")
+  (->> (travel "example.txt" commands-part-1)
        :position
        vals
        (map #(Math/abs %))
        (apply +))
 
-  (->> (travel "input.txt")
+  (->> (travel "input.txt" commands-part-1)
        :position
        vals
        (map #(Math/abs %))
        (apply +))
+  )
+
+(defn rotate-waypoint [num waypoint]
+  (case (mod num 360)
+    0   waypoint
+    90  {:x (:y waypoint)
+         :y (* -1 (:x waypoint))}
+    180 {:x (* -1 (:x waypoint))
+         :y (* -1 (:y waypoint))}
+    270 {:x (* -1 (:y waypoint))
+         :y (:x waypoint)}))
+
+(def commands-part-2
+  {"N" (fn [{:keys [position waypoint]} num]
+         {:waypoint {:x (:x waypoint)
+                     :y (+ (:y waypoint) num)}
+          :position position})
+
+   "S" (fn [{:keys [position waypoint]} num]
+         {:waypoint {:x (:x waypoint)
+                     :y (- (:y waypoint) num)}
+          :position position})
+
+   "E" (fn [{:keys [position waypoint]} num]
+         {:waypoint {:x (+ (:x waypoint) num)
+                     :y (:y waypoint)}
+          :position position})
+
+   "W" (fn [{:keys [position waypoint]} num]
+         {:waypoint {:x (- (:x waypoint) num)
+                     :y (:y waypoint)}
+          :position position})
+
+   "F" (fn [{:keys [position waypoint]} num]
+         {:position
+          {:x (+ (:x position) (* num (:x waypoint)))
+           :y (+ (:y position) (* num (:y waypoint)))}
+          :waypoint waypoint})
+
+   "L" (fn [{:keys [position waypoint]} num]
+         {:position position
+          :waypoint (rotate-waypoint num waypoint)})
+
+   "R" (fn [{:keys [position waypoint]} num]
+         {:position position
+          :waypoint (rotate-waypoint num waypoint)})})
+
+(comment
+  (->> (travel "example.txt" commands-part-2 {:x 10 :y 1})
+       :position
+       vals
+       (map #(Math/abs %))
+       (apply +))
+
+  (->> (travel "input.txt" commands-part-2 {:x 10 :y 1})
+       :position
+       vals
+       (map #(Math/abs %))
+       (apply +))
+  ;; 51298 too low
   )

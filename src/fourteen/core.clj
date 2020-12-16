6c0b158d30428046938ed068c65a9e2903366026
Russell Matney <russell.matney@gmail.com>
2020-12-16 14:39:24 -0500

    feat: day 14 part 2
    No need to parse these big ints :)

--- src/fourteen/core.clj
+++ src/fourteen/core.clj
@@ -69,3 +69,103 @@
   (run-program "input.txt")
   (sum-final-values "input.txt")
   )
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Part 2
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defn assoc-str [s n char]
+  (str (subs s 0 n) char (subs s (+ n 1) (count s))))
+
+(comment
+  (assoc-str "hello" 2 "X"))
+
+(defn expand-floating [s]
+  (loop [addrs [""]
+         src   s]
+    (if-let [next-char (some-> src first)]
+      (let [addrs
+            (if (= next-char \X)
+              (->> addrs
+                   (map (fn [addr]
+                          [(str addr 0)
+                           (str addr 1)]
+                          ))
+                   flatten)
+              (->> addrs (map (fn [addr] (str addr next-char)))))]
+        (recur addrs (rest src)))
+      addrs)))
+
+(comment
+  (first (reverse (reverse "X1101X")))
+
+  (expand-floating (reverse (reverse "X1101X")))
+
+  )
+
+(defn pad-bit-str [s]
+  (let [len    (count s)
+        pad    (- 36 len)
+        prefix (apply str (repeat pad "0"))]
+    (str prefix s)))
+
+(comment
+  (pad-bit-str "111010101010111111111"))
+
+(defn get-all-slots [bitmask slot]
+  (let [slot-str        (pad-bit-str (Integer/toBinaryString slot))
+        masked-slot-str (reduce
+                          (fn [slot-str [i char]]
+                            (case char
+                              \0 slot-str
+                              \1 (assoc-str slot-str i \1)
+                              \X (assoc-str slot-str i \X)))
+                          (apply str slot-str)
+                          (map-indexed vector bitmask))
+        ]
+    (expand-floating masked-slot-str)
+    #_(->> (expand-floating masked-slot-str)
+           (map #(Integer/parseInt % 2)))))
+
+(comment
+  (Integer/parseInt "011010001011100100000001001011100001" 2)
+
+  (format "%036d" 42)
+  (Integer/toBinaryString 42)
+  (get-all-slots
+    "000000000000000000000000000000X1001X"
+    42))
+
+(defn apply-memory-bitmask [agg slot val]
+  (let [bitmask (:bit agg)
+        slots   (get-all-slots bitmask slot)]
+    (update agg :mem
+            (fn [mem]
+              (reduce (fn [mem slot]
+                        (assoc mem slot val))
+                      mem
+                      slots)))))
+
+(defn run [agg {:keys [cmd val slot]}]
+  (case cmd
+    ;; update bitmask
+    "mask" (assoc agg :bit val)
+
+    ;; apply bitmask, write to agg
+    "mem" (apply-memory-bitmask agg slot val)))
+
+(defn program [f]
+  (let [cmds (parse-program f)]
+    (reduce run {:mem {}} cmds)))
+
+(defn sum [f]
+  (->> (program f)
+       :mem
+       vals
+       (apply +)))
+
+(comment
+  (program "example-two.txt")
+  (sum "example-two.txt")
+  (sum "input.txt")
+  )

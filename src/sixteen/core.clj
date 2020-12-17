9021c7a5f20506af7d9e8fdf9ed1c885acd5526d
Russell Matney <russell.matney@gmail.com>
2020-12-16 19:59:15 -0500

    feat: day 16 part 2.
    finally caught up.

--- src/sixteen/core.clj
+++ src/sixteen/core.clj
@@ -33,11 +33,12 @@
   (parse "input.txt"))
 
 (defn val-in-any-range? [ranges v]
-  (->> ranges
-       (filter
-         (fn [[a b]]
-           (<= a v b)))
-       first))
+  (when v
+    (->> ranges
+         (filter
+           (fn [[a b]]
+             (<= a v b)))
+         first)))
 
 (defn error-rate [f]
   (let [parsed  (parse f)
@@ -52,5 +53,89 @@
 (comment
   (error-rate "example.txt")
   (error-rate "input.txt")
+  )
 
+;; part 2
+
+
+(defn valid-tickets [f]
+  (let [parsed  (parse f)
+        ranges  (->> parsed :rules (mapcat :ranges))
+        tickets (:nearby-tickets parsed)
+        ]
+    (->> tickets
+         (remove
+           (fn [ticket]
+             (->> ticket
+                  (remove (partial val-in-any-range? ranges))
+                  first))))))
+
+(comment
+  (:rules (parse "example-two.txt"))
+  (valid-tickets "example.txt")
+  (valid-tickets "example-two.txt"))
+
+(defn possible-indexes [{:keys [ranges]} tickets]
+  (->> (range 0 (count tickets))
+       (filter
+         (fn [i]
+           (->> tickets
+                (map
+                  (fn [ticket]
+                    (if (val-in-any-range? ranges (nth ticket i nil))
+                      true
+                      false)))
+                (every? true?))))
+       (into #{})))
+
+(defn rules-with-possible-indexes [f]
+  (let [rules   (:rules (parse f))
+        tickets (valid-tickets f)]
+    (->> rules
+         (map (fn [rule]
+                (assoc rule :ixs (possible-indexes rule tickets)))))))
+
+(defn rules-with-index [f]
+  (->>
+    (rules-with-possible-indexes f)
+    (sort-by (comp count :ixs))
+    ((fn [rules]
+       (loop [with-index []
+              rules      rules]
+         (if-let [rule (some-> rules first)]
+           ;; assumes there is only one of these, which happens to be the case
+           (let [ix (first (:ixs rule))]
+             (when (> (count (:ixs rule)) 1)
+               (println "DERP"))
+             (recur
+               (conj with-index (assoc rule :ix ix))
+               (->> rules rest (map (fn [rule]
+                                      (update rule :ixs disj ix))))))
+           with-index))))))
+
+(comment
+  (rules-with-possible-indexes "example-two.txt")
+  (rules-with-index "example-two.txt")
+  )
+
+(defn ticket-with-labels [f]
+  (let [rules       (rules-with-index f)
+        rule-map    (->> rules (map (fn [r] [(:ix r) r])) (into {}))
+        your-ticket (:your-ticket (parse f))]
+    (->> your-ticket
+         (map-indexed
+           (fn [i v]
+             [(:label (rule-map i)) v]))
+         (into {}))))
+
+(comment
+  (ticket-with-labels "example-two.txt")
+  (ticket-with-labels "input.txt")
+
+  (->>
+    (ticket-with-labels "input.txt")
+    (filter (fn [[k _v]]
+              (string/starts-with? k "departure")))
+    (map second)
+    (apply *))
   )

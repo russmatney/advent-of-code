96562ff7575a55b1665a621f23627e2d1b12a6bc
Russell Matney <russell.matney@gmail.com>
2020-12-16 19:18:26 -0500

    feat: day 16 part 1

--- src/sixteen/core.clj
+++ src/sixteen/core.clj
@@ -2,17 +2,16 @@
   (:require [util :refer [input]]
             [clojure.string :as string]))
 
-
 (defn parse-rules [rules]
   (->> rules
        (map (fn [line]
-              (let [res               (re-seq #"([\w| ]+): (\d+)-(\d+) or (\d+)-(\d+)"
-                                              line)
-                    [_ label a b x y] (first res)]
+              (let [res                    (re-seq #"([\w| ]+): (\d+)-(\d+) or (\d+)-(\d+)"
+                                                   line)
+                    [_ label & range-vals] (first res)
+                    [a b x y]              (->> range-vals (map read-string))
+                    ]
                 {:label  label
-                 :ranges [[a b] [x y]]})
-              )))
-  )
+                 :ranges [[a b] [x y]]})))))
 
 (defn parse [f]
   (->> (input f)
@@ -20,24 +19,38 @@
        (remove (comp #{""} first))
        ((fn [[rules your nearby]]
           {:rules          (parse-rules rules)
-           :your-ticket    (-> your second (string/split #","))
-           :nearby-tickets (->> nearby rest (map #(string/split % #",")))}
-          ))
-       ))
+           :your-ticket    (-> your second
+                               (string/split #",")
+                               (->> (map read-string)))
+           :nearby-tickets (->> nearby
+                                rest
+                                (map (fn [line]
+                                       (->> (string/split line #",")
+                                            (map read-string)))))}))))
 
 (comment
   (parse "example.txt")
-  (parse "input.txt")
-  )
+  (parse "input.txt"))
 
-
+(defn val-in-any-range? [ranges v]
+  (->> ranges
+       (filter
+         (fn [[a b]]
+           (<= a v b)))
+       first))
 
 (defn error-rate [f]
-  (let [parsed (parse f)
-        rules  (:rules parsed)
-        ranges (->> rules (mapcat :ranges))]
-    ranges))
+  (let [parsed  (parse f)
+        rules   (:rules parsed)
+        ranges  (->> rules (mapcat :ranges))
+        tickets (:nearby-tickets parsed)
+        values  (->> tickets (mapcat (fn [x] x)))]
+    (->> values
+         (remove (partial val-in-any-range? ranges))
+         (apply +))))
 
 (comment
   (error-rate "example.txt")
+  (error-rate "input.txt")
+
   )

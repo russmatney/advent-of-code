c9789826caded94f73989a5ed650e701ce773b51
Russell Matney <russell.matney@gmail.com>
2020-12-20 12:31:30 -0500

    day 19: parts 1&2 via instaparse

--- src/nineteen/core.clj
+++ src/nineteen/core.clj
@@ -1,55 +1,23 @@
 (ns nineteen.core
   (:require [util :refer [input]]
-            [clojure.string :as string]))
+            [clojure.string :as string]
+            [instaparse.core :as insta]))
 
-(comment
-  (input "example.txt")
-  (input "input.txt")
-  (println "hello!")
-  )
-
-(defn parse-rule [line]
-  ;; TODO expand to handle non number rules
-  (when (seq line)
-    (some->> line
-             (re-seq #"(\d+): \"?([\w|\d| ]+)\"?")
-             first
-             rest
-             ((fn [[rule-number rule-defs]]
-                (when rule-number
-                  (println rule-number rule-defs)
-                  [rule-number
-                   (-> rule-defs
-                       (string/split #" \|")
-                       (->>
-                         (map (fn [rule-def]
-                                (-> rule-def
-                                    string/trim
-                                    (string/split #" ")
-                                    (->>
-                                      (map string/trim)
-                                      (map read-string)))))))]))))))
-
-(comment
-  (parse-rule "0: 4 1 5")
-  (parse-rule "0: 2 3 | 3 2")
-  (parse-rule "0: 2 3 | 3 2 | 1")
-  (parse-rule "0: \"a\"")
-  )
-
-(defn rules [f]
-  (->> f input
+(defn parser [f]
+  (->> f
+       input
        (partition-by #{""})
        (remove (comp #{""} first))
        first
-       (map parse-rule)
-       (remove nil?)
-       (into {})
+       sort
+       (string/join "\n")
+       insta/parser
        ))
 
 (comment
-  (rules "example.txt"))
-
+  (parser "example.txt")
+  ((parser "input.txt") "aaabab")
+  ((parser "example.txt") "aaabab"))
 
 (defn messages [f]
   (->> f input
@@ -57,21 +25,16 @@
        (remove (comp #{""} first))
        second))
 
-(defn valid? [rules message]
-  true)
-
-(comment
-  (let [f              "example.txt"
-        rules          (rules f)
+(defn valid-count [f]
+  (let [parse          (parser f)
         messages       (messages f)
         valid-messages (->> messages
-                            (filter (partial valid? rules)))
-        ]
-    (concat
-      ["Rules"]
-      rules
-      ;; ["Messages"]
-      ;; messages
-      ["Valid messages"]
-      valid-messages))
+                            (map parse)
+                            (remove insta/failure?))]
+    (count valid-messages)))
+
+(comment
+  (valid-count "example.txt")
+  (valid-count "input.txt")
+  (valid-count "input-two.txt")
   )

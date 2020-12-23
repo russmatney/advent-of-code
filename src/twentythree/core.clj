994713e82a4db7c4379accebad3053716e8957e9
Russell Matney <russell.matney@gmail.com>
2020-12-23 16:59:42 -0500

    feat: day 23 part 2 - runs in ~ 25s, but i got it!

--- src/twentythree/core.clj
+++ src/twentythree/core.clj
@@ -1,6 +1,5 @@
 (ns twentythree.core
-  (:require [clojure.string :as string]
-            [clojure.set :as set]))
+  (:require [clojure.string :as string]))
 
 (def input "963275481")
 (def example "389125467")
@@ -8,47 +7,15 @@
 (defn cups [s]
   (->> s (map (comp read-string str))))
 
-(defn cups-million [s]
-  (let [cs      (cups s)
-        max     (apply max cs)
-        million 1000000
-        ]
-    (concat cs (range max million))))
-
-(defn cups-million-map [s]
-  (let [cs (cups-million s)]
-    (->> cs
-         (map-indexed vector)
-         (into {}))))
-
 (comment
   (cups input)
-  (cups example)
-
-  (->
-    (cups-million example)
-    (nth 1)
-    )
-
-  (->
-    (cups-million example)
-    (nth 102346)
-    )
-
-  (->
-    (cups-million-map example)
-    (get 702346)
-    )
-
-  (mod 13 9)
-  )
+  (cups example))
 
 (defn play-round [cs ix]
   (let [ct      (count cs)
         ix      (mod ix ct)
         highest (apply max cs)
         cyc-cs  (take (* ct 2) (cycle cs))
-        ;; pre-ix-cs         (take ix cyc-cs)
 
         post-ix-cs (drop ix cyc-cs)
         current    (first post-ix-cs)
@@ -100,7 +67,6 @@
     first))
 
 (comment
-  (println "hellloo\n\n")
   (play-rounds (cups example) 100)
 
   (play-rounds (cups example) 10000000) ;; 639147258
@@ -125,74 +91,83 @@
   (->>
     (play-rounds (cups input) 100)
     after-1
-    string/join)
-  97632548
-  )
+    string/join))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Part 2
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+(defn cups-million [s]
+  (let [cs      (cups s)
+        max     (apply max cs)
+        million 1000000
+        ]
+    (concat cs (range (+ 1 max) (+ million 1)))))
+
+(defn ->val->next-val [cs]
+  (->> cs
+       (reduce
+         (fn [{:keys [last pointers]} c]
+           {:last     c
+            :pointers (assoc pointers last c)})
+         {:last     (last cs)
+          :pointers {}})
+       :pointers
+       (into {})))
 
 
+(defn take-pts [pts start n]
+  (->> (iterate pts start)
+       rest
+       (take n)))
 
 (comment
-  (do
-    (println "starting... for" example)
-    (let [in        example
-          ix->label (cups-million-map in)
-          label->ix (set/map-invert ix->label)
-          max-label (apply max (vals ix->label))
-          ]
-      (println "created ix->label and label->ix")
-      (label->ix 12346)
-      (ix->label 12337)
+  (take-pts {1 3
+             3 6
+             7 4
+             6 7} 3 4))
 
-      (loop [current-ix 0
-             max-ix     (label->ix max-label)]
-        (let [current-val    (ix->label current-ix)
-              picked-up-ixs  (range (+ current-ix 1) (+ current-ix 4))
-              picked-up-vals (->> picked-up-ixs (map ix->label))
-              current-label  (ix->label current-ix)
-              dest-val       (->> (if (= current-label 1)
-                                    max-label
-                                    (dec current-label))
-                                  (iterate (fn [x]
-                                             (let [next (dec x)]
-                                               (if (zero? next)
-                                                 max-label
-                                                 next))))
-                                  (drop-while #((set picked-up-vals) %))
-                                  first)
-              dest-ix        (label->ix dest-val)
+(defn play-rounds-2 [in max-rounds]
+  (let [;; cs                (cups in)
+        cs                (cups-million in)
+        label->next-label (->val->next-val cs)
+        max-label         (apply max (keys label->next-label))]
+    (loop [label->next-label label->next-label
+           current-label     (first cs)
+           round             0]
+      (if (= round max-rounds)
+        (take-pts label->next-label 1 9)
+        (let [picked-up       (take-pts label->next-label current-label 3)
+              first-picked-up (first picked-up)
+              last-picked-up  (last picked-up)
+              next-current    (label->next-label last-picked-up)
 
-              ;; ix-update
-              ;; (->>
-              ;;   ()
-              ;;   (merge {dest-ix dest-val}))
-              ]
-          (println
-            "current-idx" current-ix
-            "current-val" current-val
-            "\n"
-            "picked-up-ixs" picked-up-ixs
-            "picked-up-vals" picked-up-vals
-            "\n"
-            "dest-val" dest-val
-            "dest-ix" dest-ix)
+              dest       (->> (if (= current-label 1)
+                                max-label
+                                (dec current-label))
+                              (iterate (fn [x]
+                                         (let [next (dec x)]
+                                           (if (zero? next)
+                                             max-label
+                                             next))))
+                              (drop-while #((set picked-up) %))
+                              first)
+              after-dest (label->next-label dest)]
+          (recur
+            (-> label->next-label
+                (assoc current-label next-current)
+                (assoc dest first-picked-up)
+                (assoc last-picked-up after-dest))
+            next-current
+            (inc round)))))))
 
-          ;; move pickup ixs to after dest ix
-          ;; update current ix
-
-          ))))
-
-  (-> {:hi :bye} set/map-invert)
-
-
-  (->> (cups-million example)
-       (map-indexed (fn [i x] [i x]))
-       (into {})
-       ((fn [mp]
-          (get mp 623450))))
+(comment
+  (play-rounds-2 example 10)
+  (play-rounds-2 example 100)
+  (play-rounds-2 example 10000000)
+  (->> (play-rounds-2 input 10000000)
+       (take 2)
+       (apply *))
+  (play-rounds-2 input 100)
 
   )

70b11648a2677218f1099795fec11bdb39fa3e78
Russell Matney <russell.matney@gmail.com>
2022-12-02 11:31:51 -0500

    2022 day 2: initial impl

--- src/_2022/_02/core.clj
+++ src/_2022/_02/core.clj
@@ -1,1 +1,77 @@
-(ns _2022._02.core)
+(ns _2022._02.core
+  (:require [util :as util :refer [input]]
+            [clojure.string :as string]))
+
+(comment
+  (input "example.txt")
+  (input "input.txt"))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(def choice-score
+  {"X" 1
+   "Y" 2
+   "Z" 3})
+
+(def win-score
+  {["A" "X"] 3
+   ["A" "Y"] 6
+   ["A" "Z"] 0
+   ["B" "X"] 0
+   ["B" "Y"] 3
+   ["B" "Z"] 6
+   ["C" "X"] 6
+   ["C" "Y"] 0
+   ["C" "Z"] 3})
+
+(defn score [them us]
+  (+ (choice-score us)
+     (win-score [them us])))
+
+(comment
+  (score "A" "Y")
+  (score "B" "X")
+  (score "C" "Z"))
+
+(defn total [data]
+  (->> data
+       (map #(apply score %))
+       (reduce +)))
+
+(comment
+  (total (->> (input "example.txt")
+              (map #(string/split % #" "))))
+
+  (total (->> (input "input.txt")
+              (map #(string/split % #" ")))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Part 2
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(def should-play
+  {["A" "X"] "Z"
+   ["A" "Y"] "X"
+   ["A" "Z"] "Y"
+   ["B" "X"] "X"
+   ["B" "Y"] "Y"
+   ["B" "Z"] "Z"
+   ["C" "X"] "Y"
+   ["C" "Y"] "Z"
+   ["C" "Z"] "X"})
+
+(defn score-2 [them outcome]
+  (let [us (should-play [them outcome])]
+    (score them us)))
+
+(defn total-2 [data]
+  (->> data
+       (map #(apply score-2 %))
+       (reduce +)))
+
+(comment
+  (total-2 (->> (input "example.txt")
+                (map #(string/split % #" "))))
+
+  (total-2 (->> (input "input.txt")
+                (map #(string/split % #" ")))))
---
+++ src/_2022/_02/core_test.clj
@@ -0,0 +1,17 @@
+(ns _2022._02.core-test
+  (:require
+   [_2022._02.core :as sut]
+   [clojure.test :refer [testing deftest is]]))
+
+
+(deftest score-test
+  (testing "the correct score is returned"
+    (is (= 8 (sut/score "A" "Y")))
+    (is (= 1 (sut/score "B" "X")))
+    (is (= 6 (sut/score "C" "Z")))))
+
+(deftest score-2-test
+  (testing "the correct score is returned"
+    (is (= 4 (sut/score-2 "A" "Y")))
+    (is (= 1 (sut/score-2 "B" "X")))
+    (is (= 7 (sut/score-2 "C" "Z")))))

b007bb36e241ae2ba80bdc83f1c5378a3b86230d
Russell Matney <russell.matney@gmail.com>
2022-12-06 00:48:28 -0500

    2022 day 6 quick loop impl

--- src/_2022/_06/core.clj
+++ src/_2022/_06/core.clj
@@ -1,1 +1,39 @@
-(ns _2022._06.core)
+(ns _2022._06.core
+  (:require [util :as util]))
+
+(defn input [f]
+  (first (util/parse-input (str "src/_2022/_06/" f))))
+
+(comment
+  (input "example.txt")
+  (input "example2.txt")
+  (input "input.txt"))
+
+(defn first-unique
+  ([line] (first-unique line 4))
+  ([line n]
+   (loop [next   (first line)
+          rst    (rest line)
+          i      0
+          last-n []]
+
+     (if (#{n} (count (set last-n)))
+       (do
+         (println "found a winner" i)
+         i)
+       (recur
+         (first rst)
+         (rest rst)
+         (inc i)
+         (if (< (count last-n) n)
+           (conj last-n next)
+           (conj (->> last-n (drop 1) (into [])) next)))))))
+
+(comment
+  (conj (->> [1 2 3] (drop 1) (into [])) 4)
+  (first-unique (input "example.txt"))
+  (first-unique (input "example2.txt"))
+  (first-unique (input "input.txt"))
+
+  (first-unique (input "example.txt") 14)
+  (first-unique (input "input.txt") 14))

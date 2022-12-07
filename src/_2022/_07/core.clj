5e764abde1cbb4589657b4c7a5c15e0c352e3262
Russell Matney <russell.matney@gmail.com>
2022-12-07 12:10:28 -0500

    day7: clean up, remove intermediaries

--- src/_2022/_07/core.clj
+++ src/_2022/_07/core.clj
@@ -5,12 +5,9 @@
 (defn input [f]
   (->
     (slurp (str "src/_2022/_07/" f))
+    ;; split on \n\$ to grab each command + output
     (string/split #"\n?\$ ")
     (->> (remove empty?))))
-
-(comment
-  (input "input.txt")
-  )
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; cd
@@ -26,46 +23,33 @@
 (comment
   (-> ["/" "a" "b"] butlast (into []) (concat ["c"]))
 
-  (re-seq #"\/([a-z]*\/)$" "/a/b/c/d/e/")
-
-  (handle-cd {:cwd ""} "cd /")
-  (handle-cd {:cwd "/"} "cd /")
-  (handle-cd {:cwd "/a"} "cd /")
-  (handle-cd {:cwd ""} "cd a")
   (handle-cd {:cwd ["/" "a"]} "cd b")
-  (handle-cd {:cwd ["a" "b"]} "cd b")
   (handle-cd {:cwd ["a" "b"]} "cd ..")
   (handle-cd {:cwd ["aaaa" "bbb" "cc"]} "cd ..")
-  (handle-cd {:cwd ["aaaa" "bbb" "cc"]} "cd /")
-  )
+  (handle-cd {:cwd ["aaaa" "bbb" "cc"]} "cd /"))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; ls
 
 ;; parse file
 
-(defn parse-ls-file [_state ls-file]
-  (let [[size] (-> (re-seq #"^(\d+) (.*)$" ls-file)
-                   first rest)]
-    {:size (read-string size)}))
+(defn parse-ls-file [ls-file]
+  (-> (re-seq #"^(\d+) (.*)$" ls-file) first second read-string))
 
-(comment (parse-ls-file {:cwd ["/"]} "436456 s.log"))
+(comment (parse-ls-file "436456 s.log"))
 
 ;; parse dir
 
 (defn parse-ls-dir [state ls-file]
-  (let [dir (-> (re-seq #"^dir (.+)$" ls-file)
-                first last)]
+  (let [dir (-> (re-seq #"^dir (.+)$" ls-file) first last)]
     (into [] (concat (:cwd state) [dir]))))
 
 (defn handle-ls [state cmd]
   (let [contents   (-> cmd string/split-lines rest)
-        files      (->> contents
+        files-size (->> contents
                         (filter #(re-seq #"^\d" %))
-                        (map (partial parse-ls-file state))
-                        seq)
-        files-size (when (seq files)
-                     (->> files (map :size) (reduce +)))
+                        (map parse-ls-file)
+                        (reduce +))
         dirs       (->> contents
                         (filter #(re-seq #"^dir" %))
                         (map (partial parse-ls-dir state))
@@ -77,29 +61,6 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; dispatch
 
-(defn handle-command
-  [state cmd]
-  (cond
-    (re-seq #"^cd" cmd)
-    (handle-cd state cmd)
-
-    (re-seq #"^ls" cmd)
-    (handle-ls state cmd)))
-
-(comment
-  (->>
-    (input "example.txt")
-    (reduce handle-command {:cwd [] :dirs {}}))
-
-  (->>
-    (input "input.txt")
-    (reduce handle-command {:cwd [] :dirs {}})))
-
-(defn handle-commands [f]
-  (->>
-    (input f)
-    (reduce handle-command {:cwd [] :dirs {}})))
-
 (defn calc-dir-size [state-dirs dir-path]
   (let [{:keys [files-size dirs]} (get state-dirs dir-path)
         nested-size               (if (seq dirs)
@@ -107,23 +68,29 @@
                                          (map (partial calc-dir-size state-dirs))
                                          (reduce +))
                                     0)]
-    (println
-      "calc-dir-size" dir-path
-      "files-size" files-size
-      "nested-size" nested-size
-      "dirs" dirs)
     (+ (or files-size 0) (or nested-size 0))))
 
-(defn set-dir-sizes [state]
+(defn set-dir-sizes [dirs]
   (reduce
     (fn [state-dirs dir-path]
-      (println dir-path "....calcing....")
       (let [dir-size (calc-dir-size state-dirs dir-path)]
-        (println dir-path dir-size)
-        (println "\n")
         (assoc-in state-dirs [dir-path :dir-size] dir-size)))
-    (:dirs state)
-    (->> state :dirs keys)))
+    dirs
+    (->> dirs keys)))
+
+(defn handle-command
+  [state cmd]
+  (cond (re-seq #"^cd" cmd) (handle-cd state cmd)
+        (re-seq #"^ls" cmd) (handle-ls state cmd)))
+
+(defn handle-commands [f]
+  (->>
+    (input f)
+    (reduce handle-command {:cwd [] :dirs {}})
+    :dirs
+    set-dir-sizes))
+
+(comment (handle-commands "example.txt"))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; part 1
@@ -131,15 +98,13 @@
 (defn part1 [f]
   (->>
     (handle-commands f)
-    set-dir-sizes
     (filter (fn [[_path {:keys [dir-size]}]] (> 100000 dir-size)))
     (map (comp :dir-size second))
     (reduce +)))
 
 (comment
   (part1 "example.txt")
-  (part1 "input.txt")
-  *1)
+  (part1 "input.txt"))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; part 2
@@ -153,62 +118,19 @@
 (def required-space 30000000)
 
 (defn part2 [f]
-  (let [dir-sizes    (-> f handle-commands set-dir-sizes)
-        unused       (unused-space dir-sizes)
-        required     (- required-space unused)
-        large-enough (->> dir-sizes
-                          (filter (comp (fn [size] (>= size required))
-                                        :dir-size second)))]
-    (->> large-enough
+  (let [dirs     (handle-commands f)
+        required (- required-space (unused-space dirs))]
+    (->> dirs
+         (filter (comp (fn [size] (>= size required))
+                       :dir-size second))
          (sort-by (comp :dir-size second) <)
-         first)))
+         first second :dir-size)))
 
 (comment
   (part2 "example.txt")
   (part2 "input.txt")
-  ;; 4415 too low
-  ;; 4420059 too low
   ;; 5025657
-  (->>
-    "input.txt"
-    handle-commands
-    set-dir-sizes
-    #_unused-space)
+  (->> "input.txt" handle-commands
+       #_unused-space)
 
-  (get *1 ["/"])
-
-  (- 70000000 48381165)
-  (- 70000000 44274331)
-
-  44274331
-
-  (-> "input.txt"
-      handle-commands
-      set-dir-sizes
-      unused-space)
-
-  ;; all files
-  (->>
-    "input.txt"
-    handle-commands
-    :dirs
-    vals
-    (tree-seq (constantly true) :files)
-    (mapcat identity)
-    (mapcat :files))
-
-  (->>
-    "input.txt"
-    handle-commands
-    :dirs
-    vals
-    (tree-seq (constantly true) :files)
-    (mapcat identity)
-    (mapcat :files)
-    (map :size)
-    (reduce +)
-    )
-  44274331 ;; all files
-
-
-  )
+  (get *1 ["/"]))

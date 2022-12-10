4d506adcde3fd3ee5a1179d473a3fbfcb817067a
Russell Matney <russell.matney@gmail.com>
2022-12-10 13:35:25 -0500

    day 10: part 2
    Not perfect, but i was able to at least read it!

--- src/_2022/_10/core.clj
+++ src/_2022/_10/core.clj
@@ -19,17 +19,38 @@
   (input "example.txt")
   (input "example2.txt"))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(def initial-state {:x                 1
-                    :stored-cycle-vals {}
-                    :instruction       nil
-                    :rest-instructions []})
+(defn sprite-char [cycle x]
+  (let [sprite-x   (-> x (mod 40))
+        in-sprite? #{sprite-x (inc sprite-x) (inc (inc sprite-x))}
+        mod-cycle  (mod cycle 40)]
+    (if (in-sprite? mod-cycle) "#" ".")))
+
+(comment
+  (mod 1 40)
+  (mod 41 40)
+
+  (sprite-char 1 1)
+  (sprite-char 2 1)
+  (sprite-char 3 1)
+  (sprite-char 4 1)
+  (sprite-char (+ 7 40) 5)
+  (sprite-char (+ 17 40 40) (+ 16 40)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(def initial-state
+  {:x                 1
+   :stored-cycle-vals {}
+   :instruction       nil
+   :rest-instructions []
+   :img               ""})
 
 (defn set-next-instruction [state]
   (-> state
       (assoc :instruction (first (:rest-instructions state)))
-      (update :rest-instructions rest))
-  )
+      (update :rest-instructions rest)))
 
 (defn run-program
   ([prog ticks] (run-program prog ticks nil))
@@ -38,11 +59,13 @@
      (->> (range 2 (inc ticks))
           (reduce
             (fn [state cycle]
-              (println cycle (-> state (dissoc :rest-instructions :stored-cycle-vals)))
               (let [state
                     (if (and (seq store-cycle-num?) (store-cycle-num? cycle))
                       (-> state (update :stored-cycle-vals assoc cycle (:x state)))
                       state)
+
+                    state (update state :img str (sprite-char cycle (:x state)))
+
                     current-instruction (:instruction state)]
 
                 (match current-instruction
@@ -56,23 +79,27 @@
 (defn stored-signal-strengths [state]
   (->> state :stored-cycle-vals (map #(apply * %)) (reduce +)))
 
+(defn print-img [{:keys [img]}]
+  (->> img (partition 40) (map #(apply str %)) (map println)))
+
 (comment
   (run-program (input "example.txt") 6 {:store-cycle-num? #{2 4 6}})
 
-  (run-program
-    (input "example2.txt") 105
-    {:store-cycle-num? #{20 60 100 140 180 220}})
+  (->
+    (run-program
+      (input "example2.txt") 400
+      {:store-cycle-num? #{20 60 100 140 180 220}})
+    print-img)
 
-  (run-program
-    (input "example2.txt") 220
-    {:store-cycle-num? #{20 60 100 140 180 220}})
-
-  (run-program
-    (input "input.txt") 220
-    {:store-cycle-num? #{20 60 100 140 180 220}})
+  (->
+    (run-program
+      (input "input.txt") 600)
+    print-img)
+  ;; somehow dropping the first # on each, but it works
+  ;; PLGFKAZG
 
   (stored-signal-strengths *1)
+  (print-img *1)
 
   (first (input "example.txt"))
-  (first (rest (input "example.txt")))
-  )
+  (first (rest (input "example.txt"))))

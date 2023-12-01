6d4117cd23477d1d3823ed9e80bc7144e96269b6
Russell Matney <russell.matney@gmail.com>
2023-12-01 12:05:17 -0500

    2023 day 1 clean up: use an overlapping regex
    Nice little trick here to avoid the workaround i originally implemented.

--- src/_2023/_01/core.clj
+++ src/_2023/_01/core.clj
@@ -1,27 +1,10 @@
 (ns _2023._01.core
-  (:require [util :as util]
-            [clojure.string :as string]))
+  (:require [util :as util]))
 
 (defn input [fname]
   (util/parse-input (str "src/_2023/_01/" fname)))
 
-(defn line->numbers [line]
-  (let [matches (re-seq #"\d" line)
-        f       (-> matches first)
-        l       (-> matches last)]
-    (-> (str f l)
-        read-string)))
-
-(comment
-  (input "example.txt")
-  (input "input.txt")
-
-  (re-seq #"\d" "a1b2c3d4e5f")
-
-  (->>
-    (input "input.txt")
-    (map line->numbers)
-    (reduce +)))
+(def crazy-reg #"(?=(\d|one|two|three|four|five|six|seven|eight|nine))")
 
 (defn str->num [s]
   (case s
@@ -36,53 +19,29 @@
     "nine"  9
     s))
 
-(def crazy-reg #"(\d|one|two|three|four|five|six|seven|eight|nine)")
-
-(defn line->matches [line]
-  (->>
-    (reduce
-      (fn [{:as agg :keys [used last]} char]
-        (let [rest  (string/replace-first line used "")
-              match (some->> (re-seq crazy-reg rest)
-                             (map first)
-                             first)]
-
-          (cond-> agg
-            (and match
-                 ;; not required, but dedupes
-                 (not (= match last)))
-            (->
-              (update :nums concat [match])
-              (assoc :last match))
-
-            true
-            (update :used str char))))
-      {:used "" :nums [] :last nil}
-      (seq line))
-    :nums))
-
-(defn line->numbers-2 [line]
-  (let [matches (line->matches line)
-        f       (-> matches first str->num)
-        l       (-> matches last str->num)]
-    (-> (str f l)
-        (read-string))))
+(defn line->numbers
+  ([line] (line->numbers #"(\d)" line))
+  ([reg line]
+   (let [matches (->> (re-seq reg line) (map second))
+         f       (-> matches first str->num)
+         l       (-> matches last str->num)]
+     (-> (str f l) read-string))))
 
 (comment
+  (input "example.txt")
   (input "example2.txt")
   (input "input.txt")
 
-  (->> (re-seq crazy-reg
-               #_"two1nine"
-               "ddgjgcrssevensix37twooneightgt")
-       (map first))
+  (re-seq #"\d" "a1b2c3d4e5f")
+  (line->numbers "a1b2c3d4e5f")
 
-  (line->numbers-2 "two1nine")
-  (line->numbers-2 "eighttwothree")
-  (line->numbers-2 "685")
-  (line->numbers-2 "3eight2twojqsvbtftp")
-  (line->numbers-2 "ddgjgcrssevensix37twooneightgt")
+  (->> (re-seq crazy-reg "ddgjgcrssevensix37twooneightgt") (map second))
+  (line->numbers crazy-reg "two1nine")
+  (line->numbers crazy-reg "685")
+  (line->numbers crazy-reg "ddgjgcrssevensix37twooneightgt")
 
-  (->> (input "input.txt")
-       (map line->numbers-2)
-       (reduce +)))
+  ;; part 1
+  (->> "input.txt" input (map line->numbers) (reduce +))
+
+  ;; part 2
+  (->> "input.txt" input (map (partial line->numbers crazy-reg)) (reduce +)))

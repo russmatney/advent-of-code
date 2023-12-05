28ebeade7ddcd16a6479cdb967976e0a87d2ea54
Russell Matney <russell.matney@gmail.com>
2023-12-05 12:28:42 -0500

    refactor: day 5 from huge maps to a helper map-value fn

--- src/_2023/_05/core.clj
+++ src/_2023/_05/core.clj
@@ -8,44 +8,45 @@
 (defn ->seeds [data]
   (->> data first first (re-seq #"(\d+)") (map first)))
 
-(comment
-  (->seeds (input "input.txt")))
+(defn map-value [map-data val]
+  (let [lowest-data (->> map-data
+                         (filter #(<= (:src %) val))
+                         (sort-by :src >)
+                         first)]
+    (cond
+      (nil? lowest-data)              val
+      (>= val (+ (:src lowest-data)
+                 (:rng lowest-data))) val
+      :else
+      (let [diff (- val (:src lowest-data))]
+        (+ (:dest lowest-data) diff)))))
 
-(defn to-map [{:keys [src dest rng]}]
-  (reduce
-    (fn [agg next]
-      (assoc agg (+ src next) (+ dest next)))
-    {}
-    (range rng))
-  )
-
 (comment
-  (to-map {:src 98 :dest 50 :rng 2}))
+  (->> [1 49 50 51 52 97 98 99 100]
+       (map (fn [x]
+              [x (map-value
+                   [{:src 98 :dest 50 :rng 2}
+                    {:src 50 :dest 52 :rng 2}] x)]))))
 
 (defn ->seed-map [lines]
   (let [[src dest] (->> lines first (re-seq #"(\w+)-to-(\w+)") first rest)
-        digits     (->> lines rest
-                        (map (fn [line]
-                               (let [[dest src range]
-                                     (->> line
-                                          (re-seq #"(\d+)")
-                                          (map first)
-                                          (map parse-long))]
-                                 {:src src :dest dest :rng range}))))
-        maps       (map to-map digits)
-        map        (apply merge maps)]
+        map-data   (->> lines rest (map (fn [line]
+                                          (let [[dest src range]
+                                                (->> line
+                                                     (re-seq #"(\d+)")
+                                                     (map first)
+                                                     (map parse-long))]
+                                            {:src src :dest dest :rng range}))))]
     {:source      src
      :destination dest
-     :seed-map    map}))
+     :map-data    map-data}))
 
 (comment
-  (let [sm (->> (input "example.txt") rest (first) ->seed-map :seed-map)
-        v  99]
-    (sm v v)))
+  (->> (input "example.txt") rest (first) ->seed-map))
 
 (defn ->maps [data]
   (->> data (rest) (map ->seed-map)))
 
-
 (comment
-  (->maps (input "example.txt")))
+  (->maps (input "example.txt"))
+  (->maps (input "input.txt")))

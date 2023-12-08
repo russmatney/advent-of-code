a0ca7705e2f4849a6c14bd5aafe002b4fbf037c0
Russell Matney <russell.matney@gmail.com>
2023-12-08 16:32:51 -0500

    day 8 part 2 wip - naive (not finishing) approach

--- src/_2023/_08/core.clj
+++ src/_2023/_08/core.clj
@@ -1,5 +1,6 @@
 (ns _2023._08.core
-  (:require [util :as util]))
+  (:require [util :as util]
+            [clojure.string :as string]))
 
 (defn input [fname]
   (util/parse-input (str "src/_2023/_08/" fname ".txt") {:partition? true}))
@@ -16,7 +17,7 @@
         network (reduce
                   (fn [net line]
                     (let [key          (->> line (take 3) (apply str))
-                          [left right] (->> line (re-seq #"([A-Z]{3}), ([A-Z]{3})")
+                          [left right] (->> line (re-seq #"([A-Z0-9]{3}), ([A-Z0-9]{3})")
                                             (map rest) first)]
                       (assoc net key {:left left :right right})))
                   {}
@@ -50,5 +51,31 @@
   (mod 8 5)
 
   (count-steps-a-to-z "example")
-  (count-steps-a-to-z "input")
+  (count-steps-a-to-z "input"))
+
+(defn ghost-steps-a-to-z [fname]
+  (let [{:keys [steps network]} (parse fname)
+        total-steps             (count steps)
+        first-nodes             (->> network keys (filter #(string/ends-with? % "A")) (into #{}))
+        all-at-z?               (fn [nodes]
+                                  (->> nodes (remove #(string/ends-with? % "Z")) empty?))]
+    (loop [ct            1
+           step-idx      0
+           current-nodes first-nodes]
+      (let [next-dir   (case (get steps step-idx) \L :left \R :right)
+            next-nodes (->> current-nodes (map #(get (network %) next-dir)))]
+        (if (all-at-z? next-nodes)
+          ct
+          (recur
+            (inc ct)
+            (mod (inc step-idx) total-steps)
+            (into #{} next-nodes)))))))
+
+(comment
+  (ghost-steps-a-to-z "example2")
+  (ghost-steps-a-to-z "input")
+
+  (let [network (-> "example2" parse :network)]
+    (->> network keys (filter #(string/ends-with? % "A")) (into #{})
+         (map (fn [node] (network node)))))
   )

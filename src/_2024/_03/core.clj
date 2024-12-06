137fa34d0eb18dfe980cba64ab033d5dab68c8e3
Russell Matney <russell.matney@gmail.com>
2024-12-05 22:46:05 -0500

    2024 day 3 part 2

--- src/_2024/_03/core.clj
+++ src/_2024/_03/core.clj
@@ -15,11 +15,7 @@
 
 (comment
   (->> (input "input.txt") parse-ops)
-
-  (parse-ops "xmul(2,4)%&")
-  (->> "xmul(2,4)%&"
-       (re-seq #"mul\(\d{1,3},\d{1,3}\)"))
-  (->> "s34s899s9s" (re-seq #"\d{1,3}")))
+  (parse-ops "xmul(2,4)%&do()blah"))
 
 (defn calc-op [op]
   (let [[_match cmd arg1 arg2] op]
@@ -30,6 +26,24 @@
   (->> (input "example.txt") parse-ops (map calc-op) (reduce + 0))
   (->> (input "input.txt") parse-ops
        (map calc-op) (reduce + 0))
-  166630675
 
-  )
+  (->> (input "example.txt")
+       (parse-ops)))
+
+(defn calc-part-2 [s]
+  (->> s
+       (re-seq #"(mul)\((\d{1,3}),(\d{1,3})\)|(do)\(\)|(don't)\(\)")
+       (reduce
+         (fn [{:keys [on] :as agg} op]
+           (let [[_match _mul _arg1 _arg2 do dont] op]
+             (cond
+               do    (assoc agg :on true)
+               dont  (assoc agg :on false)
+               on    (update agg :sum + (calc-op op))
+               :else agg)))
+         {:on true :sum 0})))
+
+(comment
+  (->>
+    (input "input.txt")
+    (calc-part-2)))
